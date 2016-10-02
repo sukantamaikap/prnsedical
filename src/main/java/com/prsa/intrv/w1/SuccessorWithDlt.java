d3d5f61e0676b4373bc33beadf3e22a760aaf145
sukantamaikap <sukanta.maikap@gmail.com>
2016-10-02 08:45:04 +0530

    basic classes added

---
+++ src/main/java/com/prsa/intrv/w1/SuccessorWithDlt.java
@@ -0,0 +1,10 @@
+package com.prsa.intrv.w1;
+
+/**
+ * Given a set of N integers S={0,1,...,N−1} and a sequence of requests of the following form:
+ * Remove x from S
+ * Find the successor of x: the smallest y in S such that y≥x.
+ * design a data type so that all operations (except construction) should take logarithmic time or better.
+ */
+public class SuccessorWithDlt {
+}
---
+++ src/main/java/com/prsa/intrv/w1/UnionFindWithCanonical.java
@@ -0,0 +1,10 @@
+package com.prsa.intrv.w1;
+
+/**
+ * Add a method 𝚏𝚒𝚗𝚍() to the union-find data type so that 𝚏𝚒𝚗𝚍(𝚒) returns the largest element in the connected component
+ * containing i. The operations, 𝚞𝚗𝚒𝚘𝚗(), 𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚎𝚍(), and 𝚏𝚒𝚗𝚍() should all take logarithmic time or better.
+ * For example, if one of the connected components is {1,2,6,9}, then the 𝚏𝚒𝚗𝚍() method should return 9 for each of the
+ * four elements in the connected components.
+ */
+public class UnionFindWithCanonical {
+}
