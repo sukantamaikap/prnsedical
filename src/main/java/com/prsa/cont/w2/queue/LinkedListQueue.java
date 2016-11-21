2dc221ae4928bde86e17651b91a7f198b6d3c07c
sukantamaikap <sukanta.maikap@gmail.com>
2016-11-21 20:42:54 +0530

    added iterator for queue

--- src/main/java/com/prsa/cont/w2/queue/LinkedListQueue.java
+++ src/main/java/com/prsa/cont/w2/queue/LinkedListQueue.java
@@ -1,12 +1,15 @@
 package com.prsa.cont.w2.queue;
 
+import java.util.Iterator;
+
 /**
  * Queue implementation with linked list.
  */
-public class LinkedListQueue<T> {
+public class LinkedListQueue<T> implements Iterable<T> {
     private int size = 0;
     private Node head = null;
     private Node tail = null;
+
 
     private class Node {
         T data;
@@ -51,5 +54,28 @@ public class LinkedListQueue<T> {
 
     public boolean isEmpty() {
         return this.head == null && this.tail == null;
+    }
+
+    public Iterator<T> iterator() {
+        return new LinkedListQueueIterator();
+    }
+
+
+    private class LinkedListQueueIterator implements Iterator<T> {
+        Node scanner = LinkedListQueue.this.tail;
+
+        public boolean hasNext() {
+            return scanner != null;
+        }
+
+        public T next() {
+            final T t = scanner.data;
+            scanner = scanner.next;
+            return t;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
--- src/test/java/com/prsa/cont/w2/queue/LinkedListQueueTest.java
+++ src/test/java/com/prsa/cont/w2/queue/LinkedListQueueTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
- * Created by smaikap on 6/11/16.
+ * Tests for {@link LinkedListQueue}.
  */
 public class LinkedListQueueTest {
     @Test
@@ -71,6 +71,27 @@ public class LinkedListQueueTest {
         }
 
         Assert.assertEquals(0, queue.getSize(), "Size does not match expected");
+    }
+
+    @Test
+    public void testLinkedListQueueIterator() {
+        final LinkedListQueue<Integer> queue = new LinkedListQueue<Integer>();
+
+        final int size = 100;
+
+        for (int i = 0; i < size; i++) {
+            queue.enque(i);
+        }
+
+        Assert.assertEquals(size, queue.getSize(), "Size does not match expected");
+
+        int count = 0;
+        for (int i : queue) {
+            Assert.assertEquals(count, i, "Iteration does not give expected result !!!");
+            count++;
+        }
+
+        Assert.assertEquals(count, size, "for each implementation is not correct !!");
     }
 
 }
