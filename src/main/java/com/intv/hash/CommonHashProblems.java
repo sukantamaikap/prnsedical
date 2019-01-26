154a23e3f9b2440458e82311c463de7989091f05
Sukanta Maikap <sukanta.maikap@Sukantas-MacBook-Pro.local>
2019-01-26 21:16:19 +1100

    First element occurring k times in an array

--- src/main/java/com/intv/hash/CommonHashProblems.java
+++ src/main/java/com/intv/hash/CommonHashProblems.java
@@ -1,10 +1,6 @@
 package com.intv.hash;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /** TODO: Document this class. */
 public class CommonHashProblems {
@@ -42,5 +38,47 @@ public class CommonHashProblems {
     }
 
     return pairs;
+  }
+
+  /**
+   * First element occurring k times in an array
+   *
+   * Given an array of n integers. The task is to find the first element that occurs k number of times. If no element occurs k times the print -1. The distribution of integer elements could be in any range.
+   *
+   * Examples:
+   * Input : {1, 7, 4, 3, 4, 8, 7},
+   *         k = 2
+   * Output : 7
+   * Both 7 and 4 occur 2 times.
+   * But 7 is the first that occurs 2 times.
+   *
+   * Input : {4, 1, 6, 1, 6, 4},
+   *         k = 1
+   * Output : -1
+   * @param input
+   * @param k
+   * @return element if found, -1 otherwise
+   */
+  public int firstElementOccuringKTimes(final int[] input, final int k) {
+    if (input == null || input.length == 0 || input.length < k || k <= 0) {
+      return -1;
+    }
+
+    final Map<Integer, Integer> cache = new LinkedHashMap<>();
+    for (int item : input) {
+      if (cache.containsKey(item)) {
+        cache.put(item, cache.get(item) + 1);
+      } else {
+        cache.put(item, 1);
+      }
+    }
+
+    for (final Integer key : cache.keySet()) {
+      if (cache.get(key) >= k) {
+        return key;
+      }
+    }
+
+    return -1;
   }
 }
